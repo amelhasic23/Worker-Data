4f3997ca8e9c2ab1c45a92151c249018e5c91f82
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-19 10:34:01 +0100

    Commit

---
+++ Worker Data/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercises
+{
+    class WorkerData
+    {
+        static void Main(string[] args)
+        {
+            int years = 47;
+            string name = "John Lord";
+
+            Console.WriteLine($"Worker: {name}, Age: {years}");
+        }
+    }
+}
